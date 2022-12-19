0d3feff8b423b2bb0329de6db09e398439f377cb
howen <howenolan30@gmail.com>
2022-12-18 21:51:16 -0600

    basic map

--- src/main/gamePanel.java
+++ src/main/gamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,19 +10,18 @@ public class gamePanel  extends JPanel implements Runnable {
     final int originalTileSize = 16;
     final int scale = 3;
     public int tileSize = originalTileSize * scale;
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol;
-    final int screenHeight = tileSize * maxScreenRow;
+    public int maxScreenCol = 16;
+    public int maxScreenRow = 12;
+    public int screenWidth = tileSize * maxScreenCol;
+    public int screenHeight = tileSize * maxScreenRow;
 
     int FPS = 60;
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this,keyH);
     //default player posistion
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
+
 
     public gamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -73,7 +73,7 @@ public class gamePanel  extends JPanel implements Runnable {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-
+        tileM.draw(g2);
         player.draw(g2);
         g2.dispose();
 
